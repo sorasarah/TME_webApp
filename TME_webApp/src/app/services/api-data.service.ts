2e58c35aa52dba1bb6c78de9cae2abc80a2bd48d
jeyapriya <jeyapriya.mouttou@gmail.com>
2024-03-27 11:41:36 +0100

    dernier modif

--- TME_webApp/src/app/services/api-data.service.ts
+++ TME_webApp/src/app/services/api-data.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { TransactionData } from '../sold-graph/sold-graph.component';
 
 @Injectable({
   providedIn: 'root'
