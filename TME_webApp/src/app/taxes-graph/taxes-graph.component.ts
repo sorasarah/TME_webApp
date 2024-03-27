5ef9f826c0de8f39b932e3331f7f55459fa109eb
jeyapriya <jeyapriya.mouttou@gmail.com>
2024-03-27 12:07:12 +0100

    mise a jour jeya

--- TME_webApp/src/app/taxes-graph/taxes-graph.component.ts
+++ TME_webApp/src/app/taxes-graph/taxes-graph.component.ts
@@ -85,7 +85,7 @@ export class TaxesGraphComponent implements OnInit {
           },
           title: {
             display: true,
-            text: 'Impot par Année',
+            text: 'Impots par Année',
           }
         }
       }
